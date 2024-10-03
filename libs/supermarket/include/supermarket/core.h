a1b48134e0f9bc8e00553333ea73ad6715892a33
Jordan Hayes <jordanhayes98@gmail.com>
2024-10-02 22:36:16 -0400

    Created implementation of calculateCartPrice

--- libs/supermarket/include/supermarket/core.h
+++ libs/supermarket/include/supermarket/core.h
@@ -2,6 +2,7 @@
 #define SUPERMARKET_CORE_H
 
 #include <cmath>
+#include <cstdint>
 #include <functional>
 #include <string>
 #include <unordered_map>
@@ -11,6 +12,11 @@ namespace supermarket {
 
 class Price {
   public:
+    explicit Price(float value) : m_value{ value }
+    {
+        // Intentionally left blank.
+    }
+
     [[nodiscard]] constexpr float
     raw() const
     {
@@ -23,24 +29,52 @@ class Price {
         return std::round(m_value * 100.0f) / 100.0f;
     }
 
+    friend Price
+    operator+(Price const& lhs, Price const& rhs)
+    {
+        return Price{ lhs.raw() + rhs.raw() };
+    }
+
+    friend Price
+    operator+=(Price const& lhs, Price const& rhs)
+    {
+        return lhs + rhs;
+    }
+
   private:
     float m_value;
 };
 
 struct Item {
     std::string name;
-    float unitPrice;
+    Price unitPrice;
 };
 
-struct Cart {
-    std::vector<Item> items;
+using ItemCount = uint32_t;
+
+class Cart {
+  public:
+    explicit Cart(std::unordered_map<Item, ItemCount> const& items)
+        : m_items{ items }
+    {
+        // Intentionally left blank.
+    }
+
+    [[nodiscard]] std::unordered_map<Item, ItemCount>
+    getItemCounts() const
+    {
+        return m_items;
+    }
+
+  private:
+    std::unordered_map<Item, ItemCount> m_items;
 };
 
-using Discounts = std::function<float(Price const&, size_t)>;
+using Discount = std::function<Price(Price const&, size_t)>;
 
-[[nodiscard]] Price
-calculateCartPrice(Cart const& cart,
-                   std::unordered_map<Item, Discounts> const& discounts);
+[[nodiscard]] Price calculateCartPrice(
+    Cart const& cart,
+    std::unordered_map<Item, std::vector<Discount> > const& discounts);
 
 } // namespace supermarket
 
--- libs/supermarket/src/core.cpp
+++ libs/supermarket/src/core.cpp
@@ -2,4 +2,25 @@
 
 namespace supermarket {
 
-} // namespace supermarket\
+[[nodiscard]] Price
+calculateCartPrice(
+    Cart const& cart,
+    std::unordered_map<Item, std::vector<Discount> > const& discounts)
+{
+    Price total{ 0 };
+    for(auto const& [item, count] : cart.getItemCounts()) {
+        Price itemPrice{ item.unitPrice.raw() * static_cast<float>(count) };
+        if(discounts.count(item)) {
+            std::vector<Discount> itemDiscounts{ discounts.at(item) };
+            for(auto const& discount : itemDiscounts) {
+                itemPrice = discount(itemPrice, count);
+            }
+        }
+
+        total += itemPrice;
+    }
+
+    return total;
+}
+
+} // namespace supermarket
